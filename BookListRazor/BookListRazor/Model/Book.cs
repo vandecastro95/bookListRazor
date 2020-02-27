2cf6483dd09bb7a1e12eb0f8c42e5742be40ac8f
Van De Castro <vandecastro95@gmail.com>
2020-02-27 17:06:23 -0600

    pushing bookLIstRazor

---
+++ BookListRazor/BookListRazor/Model/Book.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListRazor.Model
+{
+    public class Book
+    {
+        //automatically add id as an id 
+        [Key]
+
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Author { get; set; }
+
+        public string ISBN { get; set; }
+
+
+    }
+
+}
